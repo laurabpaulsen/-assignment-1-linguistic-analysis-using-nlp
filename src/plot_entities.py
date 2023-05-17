e119adcced14a350113fb0323a48b8733aaff168
laurabpaulsen <202005791@post.au.dk>
2023-05-17 12:22:28 +0200

    update colours and docstrings

--- src/plot_entities.py
+++ src/plot_entities.py
@@ -3,33 +3,47 @@ Plots the distribution of the entities in the dataset
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 import pandas as pd
 from pathlib import Path
 
 plt.rcParams["font.family"] = "serif"
 
-def plot_entities(dfs, save_path):
 
-    fig, ax = plt.subplots(1, figsize=(10, 5), dpi = 300)
+def plot_entities(dfs, save_path = None):
+    """
+    Plots the distribution of the entities in each of the subcorpora
+
+    Parameters
+    ----------
+    dfs : dict
+        Dictionary with the dataframes for each subcorpus (key = subcorpus name)
+    save_path : str
+        Path to save the plot to. If None, the plot is not saved.
+    """
+
+    fig, ax = plt.subplots(1, figsize=(10, 6), dpi = 300)
+
+    # define colour palette
+    palette = cm.get_cmap("Paired").colors
+    palette = [palette[3], palette[0], palette[1], palette[2]]
 
     # x ticks (all keys)
     x = list(dfs.keys())
 
     ys = {}
-    # loop over columns
-    for i, col in enumerate(dfs[x[0]].columns[1:5]):
-        # y values
-        y = [df[col].mean() for df in dfs.values()]
 
-        # add to dict
+    # loop over columns and get mean for each subcorpus
+    for col in dfs[x[0]].columns[1:5]:
+        y = [df[col].mean() for df in dfs.values()]
         ys[col] = y
 
 
     # plot with bars, next to each other
     width = 0.2
     for i, col in enumerate(ys.keys()):
-        ax.bar(np.arange(len(x)) + width * i, ys[col], width, label=col.split("_")[-1])
+        ax.bar(np.arange(len(x)) + width * i, ys[col], width, label=col.split("_")[-1], color = palette[i])
 
     # set x ticks
     ax.set_xticks(np.arange(len(x)) + width, x)
@@ -42,15 +56,13 @@ def plot_entities(dfs, save_path):
 
     ax.set_title("Relative frequency per 10.000 words")
 
-
     plt.tight_layout()
 
 
     if save_path:
         plt.savefig(save_path)
+
     
-
-
 
 def main():
     path = Path(__file__)
